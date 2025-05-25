0ab10d2c902a06c6651e4846542e31730c23468f
ntstpnv <v4slvshalashov@mail.ru>
2025-05-25 08:10:45 +0500

    +=

--- streamlit.py
+++ streamlit.py
@@ -2,62 +2,62 @@ from itertools import chain
 
 from streamlit import sidebar, json, set_page_config
 
-from cache import get_log, login_data as ld
+from cache import get_log, login_data
 
 log = get_log()
 
 set_page_config(layout="wide")
 
-g = sidebar.selectbox(
-    "Учебная группа", list(log), index=None, placeholder="Нужно выбрать"
-)
+group = sidebar.selectbox("Учебная группа", list(log), index=None)
 
-tn_set = (
-    {
-        ld["tests"][ti]
-        for ti in chain.from_iterable(ti_dict.keys() for ti_dict in log[g].values())
-    }
-    if g
-    else None
-)
-tn = sidebar.selectbox("Название теста", tn_set, disabled=not g)
+if group:
+    tn = sidebar.selectbox(
+        "Название теста",
+        {
+            login_data["tests"][ti]
+            for ti in chain.from_iterable(
+                ti_dict.keys() for ti_dict in log[group].values()
+            )
+        },
+        index=None,
+    )
 
-best = sidebar.toggle("Только лучшие попытки", disabled=not g)
+    best = sidebar.toggle("Только лучшие попытки")
+    more = sidebar.toggle("Подробная информация")
 
-ge = sidebar.number_input("Граница отображения", 0, 30, step=1, disabled=not g)
-
-if g:
+    new = {}
     if best:
-        tmp = {}
-        for fn, ti_dict in log[g].items():
-            p_max, et_min = 0, 0
+        for fn, ti_dict in log[group].items():
+            p_max, et_min, k, v = 0, 0, None, None
             for ti, r_list in ti_dict.items():
-                if ld["tests"][ti] == tn:
+                if login_data["tests"][ti] == tn:
                     for r in r_list:
-                        p_max, et_min, tmp = 0, 0, {}
                         ft, et, p, m = r.split("=")
                         et, p = int(et), int(p)
-                        if p >= ge and (p_max < p or p_max == p and et_min > et):
+                        if p_max < p or p_max == p and et_min > et:
                             p_max, et_min = p, et
-                            tmp = {
-                                "Студент": fn,
-                                "Время завершения": ft,
-                                "Длительность": f"{et // 60}:{et % 60:02}",
-                                "Результат": f"{p} из 30",
-                                "Ошибки": m,
-                            }
-                    else:
-                        if tmp:
-                            json(tmp)                
+                            if more:
+                                k, v = (
+                                    f"{fn} = {ft} = {et // 60}:{et % 60:02} = {p} из 30",
+                                    m,
+                                )
+                            else:
+                                fn = " ".join(
+                                    [i for i in fn.split() if not i.isdigit()]
+                                )
+                                k, v = f"{fn} [{p}]", m
+                if k:
+                    new[k] = v
     else:
-        new = {}
-        for fn, ti_dict in log[g].items():
+        for fn, ti_dict in log[group].items():
             for ti, r_list in ti_dict.items():
-                if ld["tests"][ti] == tn:
+                if login_data["tests"][ti] == tn:
                     for r in r_list:
                         ft, et, p, m = r.split("=")
                         et, p = int(et), int(p)
-                        if p >= ge:
-                            new.setdefault(fn, {})
+                        new.setdefault(fn, {})
+                        if more:
                             new[fn][f"{ft} = {et // 60}:{et % 60:02} = {p} из 30"] = m
-        json(new)
+                        else:
+                            new[fn][f"{p} из 30"] = m
+    json(new)

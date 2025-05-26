211fef50f2dbd6069d74d45d41348e87d562b4f3
ntstpnv <v4slvshalashov@mail.ru>
2025-05-26 06:41:41 +0500

    +=

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ log = get_log()
 
 set_page_config(layout="wide")
 
-group = sidebar.selectbox("Учебная группа", list(log), index=None)
+group = sidebar.selectbox("Учебная группа", log, index=None, placeholder="Надо выбрать")
 
 if group:
     tn = sidebar.selectbox(
@@ -20,44 +20,40 @@ if group:
             )
         },
         index=None,
+        placeholder="Надо выбрать",
     )
 
     best = sidebar.toggle("Только лучшие попытки")
-    more = sidebar.toggle("Подробная информация")
+    more = sidebar.toggle("Подробная информация", disabled=not best)
 
     new = {}
     if best:
         for fn, ti_dict in log[group].items():
-            p_max, et_min, k, v = 0, 0, None, None
+            ft_, et_min, p_max, m_ = None, 0, 0, None
             for ti, r_list in ti_dict.items():
                 if login_data["tests"][ti] == tn:
                     for r in r_list:
                         ft, et, p, m = r.split("=")
                         et, p = int(et), int(p)
                         if p_max < p or p_max == p and et_min > et:
-                            p_max, et_min = p, et
-                            if more:
-                                k, v = (
-                                    f"{fn} = {ft} = {et // 60}:{et % 60:02} = {p} из 30",
-                                    m,
-                                )
-                            else:
-                                fn = " ".join(
-                                    [i for i in fn.split() if not i.isdigit()]
-                                )
-                                k, v = f"{fn} [{p}]", m
-                if k:
-                    new[k] = v
+                            ft_, et_min, p_max, m_ = ft, et, p, m
+            if ft_:
+                if more:
+                    new.setdefault(fn, {})
+                    new[fn] = {
+                        f"{ft_} = {et_min // 60}:{et_min % 60:02} = {p_max} из 30": m_
+                    }
+                else:
+                    new[" ".join([i for i in fn.split() if not i.isdigit()])] = (
+                        f"{p_max} из 30"
+                    )
     else:
         for fn, ti_dict in log[group].items():
             for ti, r_list in ti_dict.items():
                 if login_data["tests"][ti] == tn:
                     for r in r_list:
                         ft, et, p, m = r.split("=")
-                        et, p = int(et), int(p)
+                        et = int(et)
                         new.setdefault(fn, {})
-                        if more:
-                            new[fn][f"{ft} = {et // 60}:{et % 60:02} = {p} из 30"] = m
-                        else:
-                            new[fn][f"{p} из 30"] = m
+                        new[fn][f"{ft} = {et // 60}:{et % 60:02} = {p} из 30"] = m
     json(new)

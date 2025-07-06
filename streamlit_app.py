1aaa7111cab79e413aaa6c7029cc0249fcb5d221
ntstpnv <v4slvshalashov@mail.ru>
2025-07-06 22:34:12 +0500

    +=

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+from bisect import bisect_left
 from datetime import datetime
 from itertools import chain
 
@@ -18,9 +19,27 @@ def get_log() -> dict:
 
 set_page_config(layout="wide")
 
-log = get_log()
-group = sidebar.selectbox("Учебная группа", log, index=None, placeholder="Надо выбрать")
+log, new = get_log(), {}
 
+last = sidebar.toggle("Новые результаты", value=True)
+if last:
+    k_list, v_list = [0, 0, 0, 0, 0], ["", "", "", "", ""]
+    for g, g_dict in log.items():
+        for fn, ti_dict in g_dict.items():
+            for ti, r_list in ti_dict.items():
+                for r in r_list:
+                    ft, et, p, m = r.split("=")
+                    ft = int(ft)
+                    i = bisect_left(k_list, ft)
+                    if i:
+                        k_list[i - 1] = ft
+                        fn = " ".join([i for i in fn.split() if not i.isdigit()])
+                        v_list[i - 1] = f"{g} {fn} {catalog[ti]} {p}"
+    k_list = [datetime.fromtimestamp(i).strftime("%H:%M %d.%m.%y") for i in k_list[::-1]]
+    v_list = v_list[::-1]
+    new = dict(zip(k_list, v_list))
+
+group = sidebar.selectbox("Учебная группа", log, index=None, placeholder="Надо выбрать", disabled=not last)
 if group:
     tn = sidebar.selectbox(
         "Название теста",
@@ -34,10 +53,9 @@ if group:
         placeholder="Надо выбрать",
     )
 
-    best = sidebar.toggle("Только лучшие попытки")
+    best = sidebar.toggle("Только лучшие попытки", value=True)
     more = sidebar.toggle("Подробная информация", disabled=not best)
 
-    new = {}
     if best:
         for fn, ti_dict in log[group].items():
             ft_, et_min, p_max, m_ = None, 0, 0, None
@@ -67,4 +85,4 @@ if group:
                         ft = datetime.fromtimestamp(ft).strftime("%H:%M %d.%m.%y")
                         et = f"{et // 60}:{et % 60:02}"
                         new[fn][f"{ft} = {et} = {p} из 30"] = m
-    json(new)
+json(new)
